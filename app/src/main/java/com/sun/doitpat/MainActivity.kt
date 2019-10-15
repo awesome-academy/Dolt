74ed85ced7f8ecedb8d22d340e3a560f02a4cbaf
tuanpa-0908 <phuong.anh.tuan@sun-asterisk.com>
2019-10-15 11:25:19 +0700

    main activity UI created

--- app/src/main/java/com/sun/doitpat/MainActivity.kt
+++ app/src/main/java/com/sun/doitpat/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.sun.doitpat
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makeStatusBarTransparent()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun makeStatusBarTransparent() {
+        window.apply {
+            setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+            statusBarColor = Color.TRANSPARENT
+        }
     }
 }
