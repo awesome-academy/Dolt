3757eb5856b837e197a3d5e1412b4064abb4a94a
tuanpa-0908 <phuong.anh.tuan@sun-asterisk.com>
2019-10-28 22:34:12 +0700

    notification logic created

--- app/src/main/java/com/sun/doitpat/util/Constants.kt
+++ app/src/main/java/com/sun/doitpat/util/Constants.kt
@@ -8,4 +8,8 @@ object Constants {
     const val DEFAULT_ID = -1
     const val WIDGET_BROADCAST = "ToDo"
     const val WIDGET_DATA_BROADCAST = "Data"
+    const val ID = "id"
+    const val TITLE = "Title"
+    const val PLACE = "Place"
+    const val CHANNEL_ID = "08081998"
 }
